b097a373a3a703e7c73fdc494b753e1ba817464f
clemIPSA <clement.bourdeau@outlook.fr>
2024-03-20 09:37:10 +0100

    question 3

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+	int a;
+	int b;
+	printf("Entrez la valeur de a : ");
+	scanf("%d",&a);
+	printf("\nEntrez la valeur de b : ");
+        scanf("%d",&b);
+	printf("\nResultat de a+b %d",a+b);
+	printf("\n");
+	return 0;
+
+
+}
